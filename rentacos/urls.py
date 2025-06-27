beedded8315ccfbf49adab080737d3d1410b97bc
Angel Codina <angel.codin@gmail.com>
2025-06-26 20:08:47 -0400

    Fix: se agregó ruta de login para vista protegida

--- rentacos/urls.py
+++ rentacos/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +11,5 @@ urlpatterns = [
     path('', include('apps.alquiler.urls')),  # rutas de tu app alquiler
     path('flota/', include('apps.flota.urls')),  # rutas de la app flota
     path('mantenimiento/', include('apps.mantenimiento.urls')),  # rutas de la app mantenimiento
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
